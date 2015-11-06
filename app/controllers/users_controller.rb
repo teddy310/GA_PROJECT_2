8a98bec3e863582e96db4df0e286fa8193e19157
Ted Shumway <shumway.ted@gmail.com>
2015-11-06 00:34:45 -0800

    deleting products works, deleting user works but causes error in the browser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
 
-
-
   def index
     @users = User.all
   end
@@ -13,10 +11,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-      redirect_to('/')
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+      else
+        render :new
+      end
     end
 
   def edit
@@ -38,19 +41,18 @@ class UsersController < ApplicationController
     end
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path
-    else
-      render :new
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:notice] = "You have successfully deleted your account."
+      redirect_to products_path
     end
   end
+
 
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
-
 end
